f0bd79a18bda12a1da2a99f1412972bf1f65da09
msolanogithub <msolano2505@gmail.com>
2021-03-17 15:34:43 -0500

    +Add: Structure to `ipanel`

--- _config/mainPages.js
+++ _config/mainPages.js
@@ -1,85 +1,88 @@
 export default {
-	//Login
-	login: {
-		permission: null,
-		activated: true,
-		path: '/auth/login',
-		name: 'auth.login',
-		page: () => import('@imagina/quser/_pages/login'),
-		layout: () => import('@imagina/qsite/_layouts/blank.vue'),
-		title: 'quser.sidebar.login',
-		icon: 'fas fa-chart-bar',
-		authenticated: true,
-	},
+  //Login
+  login: {
+    permission: null,
+    activated: true,
+    path: '/auth/login',
+    name: 'auth.login',
+    page: () => import('@imagina/quser/_pages/login'),
+    layout: () => import('@imagina/qsite/_layouts/blank.vue'),
+    title: 'quser.sidebar.login',
+    icon: 'fas fa-chart-bar',
+    authenticated: true,
+  },
   //Logout
-	logout: {
-		permission: null,
-		activated: true,
-		path: '/auth/logout',
-		name: 'auth.logout',
-		page: () => import('@imagina/quser/_pages/logout'),
-		layout: () => import('@imagina/qsite/_layouts/blank.vue'),
-		title: 'quser.sidebar.logout',
-		icon: 'fas fa-chart-bar'
-	},
-	//Change password
-	changePassword: {
-		permission: null,
-		activated: true,
-		path: '/auth/change-password',
-		name: 'auth.change.password',
-		page: () => import('@imagina/quser/_pages/passwordChange'),
-		layout: () => import('@imagina/qsite/_layouts/blank.vue'),
-		title: 'quser.sidebar.changePassword',
-		icon: 'fas fa-chart-bar'
-	},
-	//User Profile
-	userProfile: {
-		permission: null,
-		activated: true,
-		path: '/users/me/profile',
-		name: 'user.profile.me',
-		page: () => import('@imagina/quser/_pages/profile'),
-		layout: () => import('@imagina/qsite/_layouts/master.vue'),
-		title: 'quser.sidebar.meProfile',
-		icon: 'fas fa-user',
-		authenticated: true,
-	},
-	//reset
-	resetPassword: {
-		permission: null,
-		activated: true,
-		path: '/auth/reset',
-		name: 'auth.reset.password',
-		page: () => import('@imagina/quser/_pages/resetPassword'),
-		layout: () => import('@imagina/qsite/_layouts/blank.vue'),
-		title: 'quser.sidebar.reset',
-		icon: 'fas fa-chart-bar',
-		//middleware: [auth]
-	},
+  logout: {
+    permission: null,
+    activated: true,
+    path: '/auth/logout',
+    name: 'auth.logout',
+    page: () => import('@imagina/quser/_pages/logout'),
+    layout: () => import('@imagina/qsite/_layouts/blank.vue'),
+    title: 'quser.sidebar.logout',
+    icon: 'fas fa-chart-bar'
+  },
+  //Change password
+  changePassword: {
+    permission: null,
+    activated: true,
+    path: '/auth/change-password',
+    name: 'auth.change.password',
+    page: () => import('@imagina/quser/_pages/passwordChange'),
+    layout: () => import('@imagina/qsite/_layouts/blank.vue'),
+    title: 'quser.sidebar.changePassword',
+    icon: 'fas fa-chart-bar'
+  },
+  //User Profile
+  userProfile: {
+    permission: null,
+    activated: true,
+    path: '/me/profile',
+    name: 'user.profile.me',
+    page: () => import('@imagina/quser/_pages/profile'),
+    layout: () => import('@imagina/qsite/_layouts/master'),
+    title: 'quser.sidebar.panelProfile',
+    icon: 'fas fa-user-circle',
+    authenticated: true,
+    subHeader: {
+      refresh: true
+    }
+  },
+  //reset
+  resetPassword: {
+    permission: null,
+    activated: true,
+    path: '/auth/reset',
+    name: 'auth.reset.password',
+    page: () => import('@imagina/quser/_pages/resetPassword'),
+    layout: () => import('@imagina/qsite/_layouts/blank.vue'),
+    title: 'quser.sidebar.reset',
+    icon: 'fas fa-chart-bar',
+    //middleware: [auth]
+  },
 
-	resetPasswordComplete: {
-		permission: null,
-		activated: true,
-		path: '/auth/reset/:userId/:token',
-		name: 'auth.reset-complete',
-		page: () => import('@imagina/quser/_pages/resetPasswordComplete'),
-		layout: () => import('@imagina/qsite/_layouts/blank.vue'),
-		title: 'quser.sidebar.reset',
-		icon: 'fas fa-chart-bar',
-		//middleware: [auth]
-	},
-	// Route to Register (Specific register user Rol)
-	register: {
-		permission: null,
-		activated: true,
-		path: '/auth/register',
-		name: 'auth.register',
-		page: () => import('@imagina/quser/_pages/register'),
-		layout: () => import('@imagina/qsite/_layouts/blank.vue'),
-		title: 'quser.sidebar.reset',
-		icon: 'fas fa-chart-bar',
-		//middleware: [auth]
-	},
+  resetPasswordComplete: {
+    permission: null,
+    activated: true,
+    path: '/auth/reset/:userId/:token',
+    name: 'auth.reset-complete',
+    page: () => import('@imagina/quser/_pages/resetPasswordComplete'),
+    layout: () => import('@imagina/qsite/_layouts/blank.vue'),
+    title: 'quser.sidebar.reset',
+    icon: 'fas fa-chart-bar',
+    //middleware: [auth]
+  },
+  // Route to Register (Specific register user Rol)
+  register: {
+    permission: null,
+    activated: true,
+    path: '/auth/register',
+    name: 'auth.register',
+    page: () => import('@imagina/quser/_pages/register'),
+    layout: () => import('@imagina/qsite/_layouts/blank.vue'),
+    title: 'quser.sidebar.reset',
+    icon: 'fas fa-chart-bar',
+    //middleware: [auth]
+  },
 
 }
---
+++ _config/panelSidebar.js
@@ -0,0 +1,6 @@
+const pages = config('pages') // Get Pages from config
+
+//User
+export default [
+  pages.mainquser.userProfile,//User Index
+]
