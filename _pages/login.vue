4225ba4d084bb7f686cc6389adaece800e924fd0
T-JULIAND <juliandavidbonillatorres@gmail.com>
2021-04-19 16:20:04 -0500

    Fix: quser | Validate Redirect last section

--- _pages/login.vue
+++ _pages/login.vue
@@ -7,56 +7,49 @@
 </template>
 
 <script>
-  //components
-  import formAuth from '@imagina/quser/_components/auth/form'
+//components
+import formAuth from '@imagina/quser/_components/auth/form'
 
-  export default {
-    props: {},
-    components: { formAuth },
-    beforeRouteEnter (to, from, next) {
-      next(vm => vm.checkRedirect(from))
-    },
-    mounted () {
-      this.$nextTick(function () {
-        this.checkRedirect()
-      })
-    },
-    data () {
-      return {
-        redirectTo: false
-      }
-    },
-    methods: {
-      //check if redirect to route specific
-      async checkRedirect (from = false) {
-        let route = from || await this.$cache.get.item('route.after.login')
-
-        if (route && route.name && (route.name != 'auth.logout')) {
-          this.redirectTo = route
-          //Save data of route in storage
-          this.$cache.set('route.after.login', {
-            name: route.name,
-            fullPath: route.fullPath,
-            meta: route.meta,
-            params: route.params,
-            path: route.path,
-            query: route.query
-          })
-        } else {
-          //Search route in storage
-          this.redirectTo = { name: 'app.home' }
-        }
-      },
-      //Redirect after login
-      redirect () {
-        this.$cache.remove('route.after.login')
-        this.$router.push(this.redirectTo)
+export default {
+  props: {},
+  components: {formAuth},
+  beforeRouteEnter(to, from, next) {
+    console.warn('From Vue Route', from, 'To vue route', to)
+    next(vm => vm.fromRoute = from)
+  },
+  mounted() {
+    this.$nextTick(function () {
+    })
+  },
+  data() {
+    return {
+      fromRoute: false
+    }
+  },
+  methods: {
+    //Redirect after login
+    redirect() {
+      let windowLastRoute = document.referrer //get last navigator history route
+      let origenUrl = this.$store.state.qsiteApp.baseUrl //get local origin Url
+      console.warn("windowLastRoute", windowLastRoute, "this.fromRoute", this.fromRoute)
+      //validate last navigator history route to redirect
+      if (windowLastRoute.length && (windowLastRoute.indexOf(origenUrl) >= 0) && (windowLastRoute.indexOf("login") == -1) && (windowLastRoute.indexOf("logout") == -1)) {
+        //Redirect last
+        location.href = windowLastRoute;
+        //validate last vue router history route to rediret
+      } else if (this.fromRoute && this.fromRoute.name && (this.fromRoute.name != "auth.logout") && (this.fromRoute.name != "auth.login")) {
+        //Redirect last
+        this.$router.push({name: this.fromRoute.name});
+      } else {
+        //Redirect home iadmin
+        this.$router.push({name: 'app.home'});
       }
     }
   }
+}
 </script>
 
 <style lang="stylus">
-  #authLoginRegister
-    min-height 100vh
+#authLoginRegister
+  min-height 100vh
 </style>
