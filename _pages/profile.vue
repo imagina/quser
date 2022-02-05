3fde83e29883f00e61d6593286af50a6533473c3
Michael <msolano@imaginacolombia.com>
2022-02-05 09:05:05 -0500

    +Remove: Payout from profile page

--- _pages/profile.vue
+++ _pages/profile.vue
@@ -9,7 +9,7 @@
       <div class="row q-col-gutter-md">
         <!--Menu-->
         <div id="menuContent" class="col-12 col-md-3">
-          <div class="box">
+          <div class="box box-auto-height">
             <q-tabs v-model="menuOption" vertical active-color="primary">
               <q-tab v-for="(opt, keyOpt) in menuOptions" :key="keyOpt" :name="opt.value" :label="opt.label"
                      no-caps v-if="(opt.vIf != undefined) ? opt.vIf : true"/>
@@ -64,33 +64,6 @@
                     @created="$store.dispatch('quserAuth/AUTH_UPDATE')"
                     @updated="$store.dispatch('quserAuth/AUTH_UPDATE')"/>
             </q-tab-panel>
-            <!--Payment method-->
-            <q-tab-panel name="paymentMethod">
-              <!--Title-->
-              <div class="box box-auto-height q-mb-md">
-                <div class="box-title row items-center">
-                  <q-icon name="fas fa-file-invoice-dollar" size="22px" class="q-mr-sm"/>
-                  {{ $tr('ui.label.paymentMethod') }}
-                </div>
-              </div>
-              <!--Help Caption-->
-              <div class="q-mb-md">
-                <q-banner v-if="payoutBannerHelp" rounded dense :class="payoutBannerHelp.class">
-                  <template v-slot:avatar>
-                    <q-icon :name="payoutBannerHelp.icon" color="white"/>
-                  </template>
-                  <div class="text-white">{{ payoutBannerHelp.message }}</div>
-                  <template v-slot:action v-if="payout.account && payout.account.urlPanel">
-                    <q-btn flat color="white" label="Ver mi Panel"
-                           @click="$helper.openExternalURL(payout.account.urlPanel)"/>
-                  </template>
-                </q-banner>
-              </div>
-              <!--Form-->
-              <dynamic-form v-if="payout.paymentMethod.formId && !payout.account.urlPanel"
-                            v-model="form.payout" form-type="grid"
-                            :form-id="payout.paymentMethod.formId" @submit="connectPayout"/>
-            </q-tab-panel>
           </q-tab-panels>
         </div>
       </div>
@@ -123,10 +96,6 @@ export default {
         profile: {},
         payout: {}
       },
-      payout: {
-        paymentMethod: false,
-        account: false
-      },
       mainFields: ['firstName', 'lastName']
     }
   },
@@ -157,8 +126,7 @@ export default {
         {
           label: this.$trp('ui.label.address'), value: 'address',
           vIf: this.hasPermission.addresses
-        },
-        {label: this.$tr('ui.label.paymentMethod'), value: 'paymentMethod'}
+        }
       ]
     },
     //Return User data
@@ -223,46 +191,6 @@ export default {
               }
             },
           }
-        },
-        payOut: {
-          blocks: [
-            {
-              fields: {
-                paymentMethod: {
-                  value: null,
-                  type: 'select',
-                  //colClass: 'col-12 col-md-6',
-                  props: {
-                    label: `${this.$tr('ui.label.paymentMethod')}*`,
-                    rules: [val => !!val || this.$tr('ui.message.fieldRequired')],
-                  },
-                  loadOptions: {}
-                }
-              }
-            }
-          ]
-        }
-      }
-    },
-    //help banner to payout
-    payoutBannerHelp() {
-      if (!this.payout.account) {//No added
-        return {
-          class: 'bg-amber q-mt-md',
-          icon: 'fas fa-exclamation-triangle',
-          message: this.$tr('ui.message.noRegistered')
-        }
-      } else if (this.payout.account.payoutsEnabled) {//Pending
-        return {
-          class: 'bg-green q-mt-md',
-          icon: 'fas fa-check-circle',
-          message: this.$tr('ui.message.ready')
-        }
-      } else {//Added
-        return {
-          class: 'bg-info q-mt-md',
-          icon: 'fas fa-info-circle',
-          message: this.payout.account.urlPanelMsj
         }
       }
     }
@@ -282,9 +210,7 @@ export default {
         this.$store.dispatch('qsiteApp/GET_SITE_SETTINGS'),
         this.$store.dispatch('quserAuth/AUTH_UPDATE'),
         this.setProfileFormData(),
-        this.getRoleForm(),
-        this.getPaymentMethod(),
-        this.getPayoutData()
+        this.getRoleForm()
       ])
       this.pageId = this.$uid()
       this.loading = false
@@ -381,61 +307,6 @@ export default {
       }).catch(error => {
         this.$alert.error({message: this.$tr('ui.message.recordNoUpdated')})
         this.loading = false
-      })
-    },
-    //get payment method
-    getPaymentMethod() {
-      return new Promise(resolve => {
-        //Request Params
-        let requestParams = {
-          refresh: true,
-          params: {filter: {field: 'name'}}
-        }
-        //request
-        this.$crud.show('apiRoutes.qcommerce.paymentMethods', 'icommercestripe', requestParams).then(response => {
-          this.payout.paymentMethod = response.data
-          resolve(response.data)
-        }).catch(error => resolve(error))
-      })
-    },
-    //Get payout data
-    getPayoutData() {
-      return new Promise((resolve, reject) => {
-        this.$crud.get('apiRoutes.qcommerce.stripe.accountUser', {refresh: true}).then(response => {
-          if (response.data && response.data.email) {
-            this.payout.account = response.data
-            this.form.payout.email = this.$clone(response.data.email)
-          }
-          resolve(response)
-        }).catch(error => {
-          this.form.payout = {}
-          this.payout.account = false
-          resolve(false)
-        })
-      })
-    },
-    //connect with payout
-    connectPayout() {
-      return new Promise((resolve, reject) => {
-        this.loading = true
-        this.$crud.create('apiRoutes.qcommerce.stripe.connect', this.form.payout).then(response => {
-          //Show dialog to finish register
-          this.$alert.info({
-            mode: 'modal',
-            message: this.$tr('ui.message.finishRegister'),
-            actions: [{
-              label: this.$tr('ui.label.next'),
-              color: 'green',
-              handler: () => {
-                this.$helper.openExternalURL(response.data.accountRegisterLink)
-                this.loadPage()
-              }
-            }],
-          })
-          this.loading = false
-        }).catch(error => {
-          this.loading = false
-        })
       })
     }
   }
