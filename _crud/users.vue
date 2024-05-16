f77cdcb3ffc56f5331b876bc29e0067b92fdff05
msolanogithub <msolano@imaginacolombia.com>
2024-05-16 17:24:02 -0500

    fixed filter status to users CRUD

--- _crud/users.vue
+++ _crud/users.vue
@@ -90,7 +90,7 @@ export default {
                 clearable: true
               }
             },
-            isActivated: {
+            status: {
               value: null,
               type: 'select',
               props: {
