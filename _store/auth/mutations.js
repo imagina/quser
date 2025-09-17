d2e4d28cc97df45b838313a54e19b0d5c9f163e7
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2025-09-16 19:12:23 -0500

    + v12 refresh token

--- _store/auth/mutations.js
+++ _store/auth/mutations.js
@@ -76,6 +76,6 @@ export const SET_ORGANIZATION = (state, data) => {
   state.organizationId = data
 }
 
-export const SET_REFREHING = (state, data) => {
+export const SET_REFRESHING = (state, data) => {
   state.isRefreshing = data
 }
